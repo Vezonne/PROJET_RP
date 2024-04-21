b27d35649281f1029dbaaaf87080e95eb1bbf664
Rayan <perotti.rayan@gmail.com>
2024-04-21 22:21:21 +0200

    fct 20 instance

--- main.py
+++ main.py
@@ -2,32 +2,73 @@ import time
 from utils import *
 from test import *
 
-def mean_time(n,k,cellules,verticaux,horizontaux):
-    start = time.time()
-    res = a_star_multi_robot(robots_pos, cible_pos, h, cellules, verticaux, horizontaux)
-    end = time.time()
-    return end-start
+"""def mean_time(n,k,ite):
+    temps =[]
+    temps1=[]
+    resultat=[]
+
+    i=0
+    upper_bound=15
+    while i<ite:
+        print(i)
+        cellules,verticaux,horizontaux=generateRandomInstances(n,k)
+        position_cible = None
+        position_robots = np.zeros(k, dtype=tuple)
+
+        for r in range(k):
+            r_x = np.where(cellules == r+1)[0][0]
+            r_y = np.where(cellules == r+1)[1][0]
+            position_robots[r] = (r_x,r_y)
+
+        c_x = np.where(cellules == -1)[0][0]
+        c_y = np.where(cellules == -1)[1][0]
+        position_cible = (c_x,c_y)
+        start1 = time.time()
+        shortest_path = deep_dive(cellules, verticaux, horizontaux, position_robots[0], position_cible, upper_bound)
+        end1 = time.time()
+
+        print("\nChemin trouvé:", shortest_path)
+        if shortest_path <= upper_bound:
+            temps1.append(end1-start1)
+            start = time.time()
+            res = multi_dive(cellules, verticaux, horizontaux, position_robots, position_cible, shortest_path)
+            end = time.time()
+            print("\nChemin  multi trouvé:", res)
+            if res <= upper_bound:
+                temps.append(end-start)
+                resultat.append(res)
+        i+=1
+    
+    
+    return (np.mean(temps), np.mean(resultat))"""
 
 def main(args=None):
 
     file = "log.txt"
     upper_bound = 20
 
-    # n=8
-    # k=5
-    # cellules,verticaux,horizontaux=generateRandomInstances(n,k)
+
+    """ moy=mean_time(4,2,20)
+    print("\nMouvement moyen trouvé sur 20 instances:", moy[1])
+    print("\nTemps moyen d'execution sur 20 instances:", moy[0])
+    """
+    n=8
+    k=3
+    cellules,verticaux,horizontaux=generateRandomInstances(n,k)
     
     # n=8
     # k=3
     # cellules,verticaux,horizontaux=np.asarray(test_cellules),np.asarray(test_verticaux),np.asarray(test_horizontaux)
     
-    n=4
-    k=2
-    cellules,verticaux,horizontaux=np.asarray(test_cellules2),np.asarray(test_verticaux2),np.asarray(test_horizontaux2)
+    #n=4
+    #k=2
+    #cellules,verticaux,horizontaux=np.asarray(test_cellules2),np.asarray(test_verticaux2),np.asarray(test_horizontaux2)
 
     # n, k, cellules, verticaux, horizontaux = generate_log_instance(file)
     
     # showgrid(n,cellules,verticaux,horizontaux)
+
+
 
     log_map(cellules,verticaux,horizontaux, file)
 
@@ -65,7 +106,7 @@ def main(args=None):
     else:
         print(f"Shortest path not found with deep dive and an upper bound of {upper_bound}.")
 
-    if shortest_path <= 6:
+    if shortest_path <= upper_bound:
         start = time.time()
         shortest_multi_path = multi_dive(cellules, verticaux, horizontaux, position_robots, position_cible, shortest_path)
         end = time.time()
@@ -90,6 +131,7 @@ def main(args=None):
         print(f"\nTime for A*: {end-start:.3}ms")
         print("\nPath found with A* and h1:", path_h1)
         print("size:", len(path_h1) - 1)
+
 
         cel_cp = cellules.copy()
         pos_cp = position_robots.copy()
