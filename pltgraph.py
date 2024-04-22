f6cea296988e5945045221e6bf2c2158311c258a
Vezonne <vezonne.savarit@gmail.com>
2024-04-22 19:57:55 +0200

    h2 plt

--- pltgraph.py
+++ pltgraph.py
@@ -7,8 +7,8 @@ md_time = [1.74, 2.01, 3.79, 2.6, 0.825]
 md_res = [3.4, 3, 2, 3.5, 3.4]
 ash1_time = [0.0014, 0.00499, 0.00367, 0.00501, 0.00378]
 ash1_res = [3.4, 3, 3, 3.7, 3.4]
-ash2_time = [4.55, 27.7, 56.4, 1.17, -1]
-ash2_res = [1.9,1.8, 2.1, 2.2, -1]
+ash2_time = [4.55, 27.7, 56.4, 1.17, 2.4]
+ash2_res = [1.9,1.8, 2.1, 2.2, 126]
 asm_time = [2.53, 7.11, 0.187, 2.06, 6.01]
 asm_res = [3.4, 3, 2, 3.5, 3.4]
 
@@ -23,7 +23,7 @@ plt.xlabel("n")
 plt.ylabel("Time (s)")
 plt.legend()
 plt.grid()
-plt.savefig("\img\pltgraph_time.png")
+plt.savefig(".\img\pltgraph_time.png")
 
 plt.figure()
 plt.plot(n, dd_res, label="DD")
@@ -36,4 +36,4 @@ plt.xlabel("n")
 plt.ylabel("Size")
 plt.legend()
 plt.grid()
-plt.savefig("\img\pltgraph_res.png")
+plt.savefig(".\img\pltgraph_res.png")
